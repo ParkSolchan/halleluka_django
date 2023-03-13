5ec2966d54f882ee2c8ee39c722d428d8c0bbab9
Solchan, Park <solchan0106@naver.com>
2023-03-13 14:18:03 +0900

    FBV로 블로그 포스트 상세 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('<int:pk>/', views.single_post_page),
     # 입력된 url이 blog/ 로 끝난다면 참조한 views.py 에 정의되어 있는 index() 함수를 사용하겠다는 의미
     path('', views.index),
 ]
--- blog/views.py
+++ blog/views.py
@@ -14,3 +14,16 @@ def index(request) :
             'posts': posts
         }
     )
+
+def single_post_page(request, pk) :
+    # .get(pk=pk) : 괄호안의 조건을 만족하는 Post레코드를 가져오는 함수
+    #  ==> Post 모델의 pk 필드값이 매개변수 pk와 같은 레코드만을 호출하라는 의미
+    post = Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        'blog/single_post_page.html',
+        {
+            'post': post,
+        }
+    )
