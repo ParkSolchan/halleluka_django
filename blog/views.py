db37680f7f8e8a19878a8ad28ae2cc0479c17eeb
Solchan, Park <solchan0106@naver.com>
2023-03-13 14:41:05 +0900

    CBV로 블로그 포스트 상세 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 # models.py 에 선언된 Post 객체를 임포트
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 
@@ -28,15 +28,20 @@ class PostList(ListView):
 #         }
 #     )
 
-def single_post_page(request, pk) :
-    # .get(pk=pk) : 괄호안의 조건을 만족하는 Post레코드를 가져오는 함수
-    #  ==> Post 모델의 pk 필드값이 매개변수 pk와 같은 레코드만을 호출하라는 의미
-    post = Post.objects.get(pk=pk)
+# django에서 제공하는 DetailView는 '_detai'로 끝나는 파일을 이용함
+class PostDetail(DetailView) :
+    model = Post
 
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post': post,
-        }
-    )
+
+# def single_post_page(request, pk) :
+#     # .get(pk=pk) : 괄호안의 조건을 만족하는 Post레코드를 가져오는 함수
+#     #  ==> Post 모델의 pk 필드값이 매개변수 pk와 같은 레코드만을 호출하라는 의미
+#     post = Post.objects.get(pk=pk)
+
+#     return render(
+#         request,
+#         'blog/single_post_page.html',
+#         {
+#             'post': post,
+#         }
+#     )
