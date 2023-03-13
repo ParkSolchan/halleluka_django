43108f63c57ef2c6c200f8192e0ae679db094f62
Solchan, Park <solchan0106@naver.com>
2023-03-13 19:59:39 +0900

    태그 기능 추가

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Category
+from .models import Post, Category, Tag
 
 admin.site.register(Post)
 # Register your models here.
@@ -7,5 +7,8 @@ admin.site.register(Post)
 class CategoryAdmin(admin.ModelAdmin) :
     # Category 모델의 name 필드에 값이 입력됐을때, 자동으로 slug가 만들어지는 코드
     prepopulated_fields = {'slug': ('name', )}
+class TagAdmin(admin.ModelAdmin) :
+    prepopulated_fields = {'slug': ('name', )}
 
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
--- blog/models.py
+++ blog/models.py
@@ -18,6 +18,15 @@ class Category(models.Model) :
     # 복수형을 직접 지정
     class Meta:
         verbose_name_plural = 'Categories'
+class Tag(models.Model) :
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self) :
+        return self.name
+
+    def get_absolute_url(self) :
+        return f'/blog/tag/{self.slug}/'
 
 class Post(models.Model) :
     title = models.CharField(max_length=30)
@@ -37,6 +46,9 @@ class Post(models.Model) :
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+
+    # ManyToManyField 는 null=True 가 기본값
+    tags = models.ManyToManyField(Tag, blank=True)
     def __str__(self) :
         return f'[{self.pk}]{self.title} :: {self.author}'
 
