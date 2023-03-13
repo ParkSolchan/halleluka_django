9f25645a2fa047d307caa69d6f7b06995a759851
Solchan, Park <solchan0106@naver.com>
2023-03-13 14:36:50 +0900

    CBV로 블로그 포스트 목록 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # /blog/ 뒤에 정수(int) 형태의 값이 붙는 url이라면 입력받은 정수 형태의 값을 pk로 받아서 single_post_page()함수로 보내겠다는 의미
     path('<int:pk>/', views.single_post_page),
     # 입력된 url이 blog/ 로 끝난다면 참조한 views.py 에 정의되어 있는 index() 함수를 사용하겠다는 의미
-    path('', views.index),
+    # path('', views.index),
+
+    # url이 /blog/인 경우 PostList 클래스에서 처리하도록 호출
+    path('', views.PostList.as_view()),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,32 @@
 from django.shortcuts import render
 # models.py 에 선언된 Post 객체를 임포트
 from .models import Post
+from django.views.generic import ListView
 
 # Create your views here.
-def index(request) :
-    # order_by를 사용하면 pk값의 역순으로 정렬됨
-    posts = Post.objects.all().order_by('-pk');
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts
-        }
-    )
+# ListView를 사용하는데 model은 Post이다.
+class PostList(ListView):
+    # django에서 제공하는 ListView 클래스는 모델명 뒤에 '_list'가 붙은 html파일을 기본 템플릿으로 사용하도록 설정되어 있다.
+    # 즉 Post모델을 사용하면 post_list.html을 불러오는데 이를 해결하는 방법은
+    # 1. post_list.html을 만들어서 그대로 사용하는 방법
+    # 2. PostList클래스에서 template_name을 지정해주는 방법
+    model = Post
+    # template_name = 'blog/index.html'
+    # pk값을 오름차순으로 보여주라는 명령어
+    ordering = '-pk'
+    
+# def index(request) :
+#     # order_by를 사용하면 pk값의 역순으로 정렬됨
+#     posts = Post.objects.all().order_by('-pk');
+
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#             'posts': posts
+#         }
+#     )
 
 def single_post_page(request, pk) :
     # .get(pk=pk) : 괄호안의 조건을 만족하는 Post레코드를 가져오는 함수
