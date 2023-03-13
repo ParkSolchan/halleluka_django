73f0761680590d908dc8b4e53833729fa2bacf7d
Solchan, Park <solchan0106@naver.com>
2023-03-13 20:09:36 +0900

    태그로 검색기능 추가

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 # models.py 에 선언된 Post 객체를 임포트
-from .models import Post, Category
+from .models import Post, Category, Tag
 from django.views.generic import ListView, DetailView
 
 # Create your views here.
@@ -74,5 +74,20 @@ def category_page(request, slug) :
             'categories': Category.objects.all(),
             'no_category_post_count': Post.objects.filter(category=None).count(),
             'category': category,
+        }
+    )
+
+def tag_page(request, slug) :
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
+    
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'tag': tag,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
         }
     )
