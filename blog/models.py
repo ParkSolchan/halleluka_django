b354fab5702011d7d9c432ad46345acbcb382518
Solchan, Park <solchan0106@naver.com>
2023-03-14 20:46:42 +0900

    사용자 아바타 구현

--- blog/models.py
+++ blog/models.py
@@ -79,3 +79,10 @@ class Comment(models.Model) :
 
     def get_absolute_url(self) :
         return f'{self.post.get_absolute_url()}#comment-{self.pk}'
+
+    def get_avatar_url(self) :
+        if self.author.socialaccount_set.exists() :
+           return self.author.socialaccount_set.first().get_avatar_url()
+        else :
+            return f'https://doitdjango.com/avatar/id/1469/be9027120fdb9b94/svg/{self.author.email}'
+        
