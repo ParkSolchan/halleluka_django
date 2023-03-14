56ec9689a0a09a8f7133641b7c13ee41aff2d6de
Solchan, Park <solchan0106@naver.com>
2023-03-14 14:31:09 +0900

    Content 입력시 markdownx 적용하기

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from markdownx.models import MarkdownxField
+from markdownx.utils import markdown
 import os
 
 class Category(models.Model) :
@@ -31,7 +33,8 @@ class Tag(models.Model) :
 class Post(models.Model) :
     title = models.CharField(max_length=30)
     hook_text = models.CharField(max_length=100, blank=True)
-    content = models.TextField()
+    # content = models.TextField()
+    content = MarkdownxField()
 
     # blank=True :  해당 항목은 필수값이 아니다는 선언
     head_image = models.ImageField(upload_to='blog/image/%Y/%m/%d/', blank=True)
@@ -60,3 +63,6 @@ class Post(models.Model) :
     
     def get_file_ext(self) :
         return self.get_file_name.split('.')[-1]
+
+    def get_content_markdown(self) :
+        return markdown(self.content)
